ecfd37d1c6d9a43b093ee7247d43870d6b89c4c8
Javkhlanbaatar <javhaa.add@gmail.com>
2022-04-22 10:37:39 +0800

    lab4

--- Lab4/src/GradeManager.java
+++ Lab4/src/GradeManager.java
@@ -1,66 +1,113 @@
-package grades;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
-/** 
- * A GradeManager will create a command-line prompt that will let someone add grades
- * and display grades in histogram format.
+/**
+ * A GradeManager will create a command-line prompt that will let someone add
+ * grades and display grades in histogram format.
  * 
  */
 
 public class GradeManager {
-	
+
 	// Keeps track of the number of each grade this has
-	private HashMap<LetterGrade,Integer> allGrades;
+	public static HashMap<LetterGrade, Integer> allGrades;
 
 	/**
 	 * Creates a new GradeManager.
 	 */
 	public GradeManager() {
 		// Create a new HashMap of the grades
-		this.allGrades = new HashMap<LetterGrade,Integer>();
-		
+		this.allGrades = new HashMap<LetterGrade, Integer>();
+
 		// Add in all grades and set the occurance to 0
 		for (LetterGrade gl : LetterGrade.values()) {
 			allGrades.put(gl, 0);
 		}
 	}
-		
+
 	/**
 	 * Adds grade to this GradeManager.
+	 * 
 	 * @param grade - grade to add to this grad manager
 	 */
-	public void addGrade(String grade) throws InvalidGradeException {
+	public static void addGrade(String grade) throws InvalidGradeException {
+		int i = 1;
 		if (grade.equals("a")) {
-			// TODO: YOUR CODE HERE
+			LetterGrade a = LetterGrade.A;
+			int oldGrade = allGrades.get(a);
+			allGrades.put(a, oldGrade + i);
 		} else if (grade.equals("b")) {
-			// TODO: YOUR CODE HERE
+			LetterGrade b = LetterGrade.B;
+			int oldGrade = allGrades.get(b);
+			allGrades.put(b, i);
 		} else if (grade.equals("c")) {
-			// TODO: YOUR CODE HERE
-		} 		
-		// TODO: Add more cases in here
-		
-		// If grade doesn't match a valid grade, throw an InvalidGradeException
+			LetterGrade c = LetterGrade.C;
+			int oldGrade = allGrades.get(c);
+			allGrades.put(c, oldGrade + i);
+		} else if (grade.equals("d")) {
+			LetterGrade d = LetterGrade.D;
+			int oldGrade = allGrades.get(d);
+			allGrades.put(d, oldGrade + i);
+		} else if (grade.equals("f")) {
+			LetterGrade f = LetterGrade.F;
+			int oldGrade = allGrades.get(f);
+			allGrades.put(f, oldGrade + i);
+		} else // add/fdfd
+
+			throw new InvalidGradeException("Grade is not acceptable ");
+	}
+
+	public static void addP(float grade) throws InvalidGradeException {
+		int i = 1;
+		if (grade <= 4 && grade >= 3.5) {
+			LetterGrade a = LetterGrade.A;
+			int oldGrade = allGrades.get(a);
+			allGrades.put(a, oldGrade + i); // umnux utga deer n 1 eer nemegduulne ZASAX
+
+		} else if (grade >= 2.7 && grade < 3.5) {
+			LetterGrade b = LetterGrade.B;
+			int oldGrade = allGrades.get(b);
+			allGrades.put(b, i);
+		} else if (grade < 2.7 && grade > 1.7) {
+			LetterGrade c = LetterGrade.C;
+			int oldGrade = allGrades.get(c);
+			allGrades.put(c, oldGrade + i);
+		} else if (grade < 1.8 && grade > 0.7) {
+			LetterGrade d = LetterGrade.D;
+			int oldGrade = allGrades.get(d);
+			allGrades.put(d, oldGrade + i);
+		} else if (grade < 0.8 && grade >= 0) {
+			LetterGrade f = LetterGrade.F;
+			int oldGrade = allGrades.get(f);
+			allGrades.put(f, oldGrade + i);
+		} else
+			throw new InvalidGradeException(grade);
 	}
 
 	/**
 	 * Prints out a histogram of the grades to the console.
 	 *
 	 */
-	public void printHistogram() {
-		// TODO: YOUR CODE HERE
-		throw new RuntimeException("GradeManger.printHistogram() not yet implemented!");
+	public static void printHistogram() {
+		try {
+			System.out.println(getHistString());
+		} catch (Exception e) {
+			throw new RuntimeException(e + "GradeManger.printHistogram() not yet implemented!");
+		}
 	}
-	
+
 	/**
 	 * Returns a string representation of the histogram of the grades.
+	 * 
 	 * @return a string representation of the histogram of the grades.
 	 */
-	public String getHistString() {
+	public static String getHistString() {
 		StringBuffer sb = new StringBuffer();
 		for (LetterGrade gl : LetterGrade.values()) {
-			sb.append( gl+":");
-			for (int i = 0; i < this.allGrades.get(gl); i++) {
+			sb.append(gl + ":");
+			for (int i = 0; i < allGrades.get(gl); i++) {
 				sb.append("*");
 			}
 			sb.append("\n");
@@ -69,31 +116,59 @@ public class GradeManager {
 	}
 
 	/**
-	 * Simple loop that accepts 3 commands from System.in:
-	 *    add <some grade> : for example, "add a" or "add b"
-	 *                       adds the given grade to the GradeManager
-	 *    print            : prints out all the grades in this GradeManager
-	 *                       in a histogram format
-	 *    exit             : exits the program
+	 * Simple loop that accepts 3 commands from System.in: add <some grade> : for
+	 * example, "add a" or "add b" adds the given grade to the GradeManager print :
+	 * prints out all the grades in this GradeManager in a histogram format exit :
+	 * exits the program
+	 * 
 	 * @param args
+	 * @throws Exception
 	 */
-//	public static void main(String[]  args) {
-//		GradeManager gm = new GradeManager();
-//		
-//		BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
-//		System.out.println("Starting the grade manager");
-//		
-//		while (true) {
-//				String input = cin.readLine();
-//				if (input.startsWith("add")) {
-//					// TODO: YOUR CODE HERE
-//				} else if (input.equals("print")) {
-//					// TODO: YOUR CODE HERE
-//				}  else if (input.equals("exit")) {
-//					break;
-//				}
-//			
-//		}
-//	}
+
+	public static void main(String[] args) throws Exception {
+		GradeManager gm = new GradeManager();
+
+		BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Starting the grade manager");
+		System.out.println("\t Input format is   command/grade ");
+
+		while (true) {
+			String input = null;
+			String[] grd = null;
+			int dun;
+			System.out.println("\t Insert a input");
+			try {
+
+				input = cin.readLine();
+				grd = input.split("/");
+
+				if (input.startsWith("add")) {
+					addGrade(grd[1]);
+
+				} else if (input.startsWith("point")) {
+
+					addP(Float.parseFloat(grd[1])); // point/3.3
+
+				} else if (input.equals("print")) {
+					printHistogram();
+
+				} else if (input.equals("exit")) {
+
+					break;
+				}
+			} catch (NumberFormatException e) { // point/hfghhgf
+
+				throw new NumberFormatException(e + "  has an invalid format!");
+			} catch (ArrayIndexOutOfBoundsException e) { // add////
+
+				throw new ArrayIndexOutOfBoundsException(e + " index is out of bounds !!!");
+			} catch (Exception e) {
+				e.printStackTrace();
+				e.getCause();
+				throw new Exception("Value " + input + " has an invalid format!!!!!");
+			}
+		}
+		cin.close();
+	}
 
 }
